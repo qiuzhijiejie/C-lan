83f0b2962b9c13b0db15203fc4cd96263167c77b
qiuzhijiejie <3075096335@qq.com>
2024-02-27 17:39:37 +0800

    二维数组作业

---
+++ 二维数组/text.c
@@ -0,0 +1,60 @@
+#include<stdio.h>
+
+/*int main()      9×9乘法表
+{
+	int i, j;
+	for (i = 1; i < 9; i++)
+	{
+		for (j = 1; j <=i; j++)
+			printf(" % d * %d =%d\t", j, i, i * j);
+		printf("\n");
+	}
+	return 0;
+} */  
+
+
+
+//int main()//24和18的最大公约数
+//{
+//    int a = 24;
+//	int b = 18;
+//	int c = a % b;
+//	while (c != 0)
+//	{
+//		a = b;
+//		b = c;
+//		c=a % b;
+//        printf("%d", b);
+//	}
+//return 0;
+//}
+
+
+//int main()//1-10的平均数
+//{
+//	int arr[10] = { 0 };
+//	for (int i = 0; i < 10; i++) 
+//	{
+//		scanf_s("%d", &arr[i]);
+//	}
+//    int sum=0;
+//	int i;
+//	for (i = 0; i < 10; i++)
+//	{
+//	}
+//    printf("sum = % d\n", sum);
+//    double avg = sum * 1.0 / 10;
+//    printf("avg=%lf\n", avg);
+//    return 0;
+//}
+
+
+
+
+
+
+
+
+
+
+
